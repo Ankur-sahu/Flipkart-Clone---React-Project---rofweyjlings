e1476ab39f8e45d800c182198d855e882c6a7c62
Ankur sahu <ankurmaildx@gmail.com>
2023-03-22 01:09:15 +0530

    code completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+reportWebVitals();
